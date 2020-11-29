5e12ebfccd75e617723be739116ba32781d11244
Jules Singer <singerjules.perso@gmail.com>
2020-11-29 14:24:50 +0100

    add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About Us | Hello City</title>
 
     </head>
     <body>
-        <h1>Hello from France !</h1>
-        <p>It's currently {{ date('h:i A') }}.</p>
-
+      
+        <p>Built with &hearts; by Jules Singer</p>
+        <a href="/">Back to the home page</a>
         <footer>
             <p>&copy; Copyright {{ date('Y') }} &middot; <a href="/about">About Us</a></p>
         </footer>
